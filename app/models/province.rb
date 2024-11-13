65ad2a120491525627db5db2906fe47760387d5f
Noah Yanga <noag29@hotmail.com>
2024-11-12 22:26:23 -0600

    Add validations and associations to Province

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,8 @@
 class Province < ApplicationRecord
+  has_many :addresses
+
+  validates :name, presence: true
+  validates :pst_rate, numericality: { greater_than_or_equal_to: 0 }
+  validates :gst_rate, numericality: { greater_than_or_equal_to: 0 }
+  validates :hst_rate, numericality: { greater_than_or_equal_to: 0 }
 end
