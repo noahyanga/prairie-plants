e064ae97a430553dda5c38262b554b68e127cc66
Noah Yanga <noag29@hotmail.com>
2024-11-12 20:15:41 -0600

    Add validations and associations to Address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ApplicationRecord
   belongs_to :province
+  has_one :user
+
+  validates :city, presence: true
+  validates :postal_code, presence: true, format: { with: /\A[A-Za-z0-9\s\-]+\z/, message: "must be a valid postal code" }
+  validates :street_address, presence: true
+  validates :province_id, numericality: { only_integer: true }
 end
