75ab48e64213f0f5040ef51191b9b9494c0fd982
Noah Yanga <noag29@hotmail.com>
2024-11-12 20:02:05 -0600

    Add User validation and try to fix GitHub Actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-  belongs_to :address
+  belongs_to :address, optional: true
+  has_many :orders
+
+  validates :name, presence: true
+  validates :password, presence: true, length: {minimum: 5}
+  validates :email, presence: true, format {with: URI::MailTo::EMAIL_REGEXP}
+  validates :role, presence: true, inclusion: {in: %w[admin customer]}
 end
