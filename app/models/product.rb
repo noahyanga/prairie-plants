c0640e1ababf96355324449fb4d5b9c0dcab4965
Noah Yanga <noag29@hotmail.com>
2024-11-12 20:13:28 -0600

    Add Product validations and associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  has_many :images
+  has_many :product_categories
+  has_many :categories, through: :product_categories
+  has_many :order_products
+  has_many :orders, through: :order_products
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
