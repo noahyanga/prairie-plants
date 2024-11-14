067448ab7dfd29da2c4b4b7caddf10b130208cee
Noah Yanga <noag29@hotmail.com>
2024-11-14 17:17:26 -0600

    Add Order validations and associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :order_products
+  has_many :products, through: :order_products
+
+  validates :user_id, numericality: { only_integer: true }
+  validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :payment_id, presence: true
 end
