0ec3ceb5e2cc4e479922e1675a07a290a90da38f
Noah Yanga <noag29@hotmail.com>
2024-11-14 17:21:42 -0600

    Add Category and Image validations and associations

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ApplicationRecord
   belongs_to :product
+    
+  validates :image_url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "must be a valid URL" }
 end
