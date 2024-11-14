5943c34159697b3736f8111404f9359290188a25
Noah Yanga <noag29@hotmail.com>
2024-11-14 16:55:26 -0600

    #7 Fix CI environment setup bu correcting typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :password, presence: true, length: {minimum: 5}
-  validates :email, presence: true, format {with: URI::MailTo::EMAIL_REGEXP}
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :role, presence: true, inclusion: {in: %w[admin customer]}
-  validates :address_id, numbericality: {only_integer: true}
+  validates :address_id, numericality: {only_integer: true}
 end
