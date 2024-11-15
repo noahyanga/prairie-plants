ba6e3b10f31298ed7afeb0f89319931a32c37c77
Noah Yanga <noag29@hotmail.com>
2024-11-14 18:18:53 -0600

    Add migration and fix rubocop formatting

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,8 @@
 class Image < ApplicationRecord
   belongs_to :product
-  validates :image_url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "must be a valid URL" }
+
+  validates :image_url, presence: true, format: {
+    with:    URI::DEFAULT_PARSER.make_regexp,
+    message: "must be a valid URL"
+  }
 end
