4a99948285c7751a1dc59a8acd8e746e7a474b37
Noah Yanga <noag29@hotmail.com>
2024-11-29 11:26:47 -0600

    Add breadcrumbs

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,12 @@
 class Category < ApplicationRecord
   has_many :product_categories
   has_many :products, through: :product_categories
+  belongs_to :parent, class_name: "Category", optional: true
+  has_many :children, class_name: "Category", foreign_key: "parent_id"
   validates :name, presence: true
   validates :description, presence: true
+
+  def ancestors
+    parent ? parent.ancestors + [parent] : []
+  end
 end
