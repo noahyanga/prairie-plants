1131edc111341e2e1c2d3ea0acfe826dd2e636cd
Noah Yanga <noag29@hotmail.com>
2024-11-29 14:21:40 -0600

    Troubleshoot heroku build

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module PrairiePlants
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.initialize_on_precompile = false
+
   end
 end
